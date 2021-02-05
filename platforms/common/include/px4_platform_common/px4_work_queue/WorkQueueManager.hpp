6419a51b3fab1fb9c38db45e782b1e257b25126d
Daniel Agar <daniel@agar.ca>
2021-02-05 12:47:31 -0500

    PX4IO: Mix on FMU side
    Using mixers on the IO side had a remote benefit of being able to
    override all control surfaces with a radio remote on a fixed wing.
    This ended up not being used that much and since the original design
    10 years ago (2011) we have been able to convince ourselves that the
    overall system stability is at a level where this marginal benefit,
    which is not present on multicopters, is not worth the hazzle.

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -77,16 +77,16 @@ static constexpr wq_config_t hp_default{"wq:hp_default", 1900, -18};
 
 static constexpr wq_config_t uavcan{"wq:uavcan", 3624, -19};
 
-static constexpr wq_config_t UART0{"wq:UART0", 1400, -21};
-static constexpr wq_config_t UART1{"wq:UART1", 1400, -22};
-static constexpr wq_config_t UART2{"wq:UART2", 1400, -23};
-static constexpr wq_config_t UART3{"wq:UART3", 1400, -24};
-static constexpr wq_config_t UART4{"wq:UART4", 1400, -25};
-static constexpr wq_config_t UART5{"wq:UART5", 1400, -26};
-static constexpr wq_config_t UART6{"wq:UART6", 1400, -27};
-static constexpr wq_config_t UART7{"wq:UART7", 1400, -28};
-static constexpr wq_config_t UART8{"wq:UART8", 1400, -29};
-static constexpr wq_config_t UART_UNKNOWN{"wq:UART_UNKNOWN", 1400, -30};
+static constexpr wq_config_t UART0{"wq:UART0", 1632, -21};
+static constexpr wq_config_t UART1{"wq:UART1", 1632, -22};
+static constexpr wq_config_t UART2{"wq:UART2", 1632, -23};
+static constexpr wq_config_t UART3{"wq:UART3", 1632, -24};
+static constexpr wq_config_t UART4{"wq:UART4", 1632, -25};
+static constexpr wq_config_t UART5{"wq:UART5", 1632, -26};
+static constexpr wq_config_t UART6{"wq:UART6", 1632, -27};
+static constexpr wq_config_t UART7{"wq:UART7", 1632, -28};
+static constexpr wq_config_t UART8{"wq:UART8", 1632, -29};
+static constexpr wq_config_t UART_UNKNOWN{"wq:UART_UNKNOWN", 1632, -30};
 
 static constexpr wq_config_t lp_default{"wq:lp_default", 1920, -50};
 
